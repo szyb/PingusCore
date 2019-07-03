d3c9b522859bc689c3a5fa88f1aa3aa7d1a822b7
Szymon Belus <sbelus@gmail.com>
2019-07-03 21:55:22 +0200

    Add assembly information (version 0.1)

---
+++ AssemblyInfo.cs
@@ -0,0 +1,5 @@
+using System.Reflection;
+[assembly: AssemblyProduct("PingusCore")]
+[assembly: AssemblyTitle("Pinguś for .NET Core 2.2")]
+[assembly: AssemblyCopyright("Szymon Bełus © 2019")]
+[assembly: AssemblyVersion("0.1.0.0")]
